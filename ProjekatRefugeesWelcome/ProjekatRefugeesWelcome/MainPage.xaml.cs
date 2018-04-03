a6eca46976cc00711767e0f0aaf68df074c88d19
Sejla Pljakic <spljakic1@etf.unsa.ba>
2018-04-03 23:26:10 +0200

    UWP projekat

--- ProjekatRefugeesWelcome/ProjekatRefugeesWelcome/MainPage.xaml.cs
+++ ProjekatRefugeesWelcome/ProjekatRefugeesWelcome/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace ProjekatRefugeesWelcome
         {
             this.InitializeComponent();
         }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
